f1992038730657c7659859dc6c592f691f95fed5
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2025-04-13 01:00:28 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,12 @@
 import { jsx } from 'hono/jsx'
 import Layout from './Layout'
-import Header from './Header'
 import Footer from './Footer'
-import { sentryIds, gaIds } from '../constants'
 
 const App = (props) => {
   const { env, country } = props
-  const sentryId = sentryIds[env]
-  const gaId = gaIds[env]
   return (
-    <Layout country={country} sentryId={sentryId} gaId={gaId}>
-      <div class='content'>
-        <Header />
-        <Footer />
-      </div>
-    </Layout>
+    <Layout country={country}>
+<div class='content'> <Footer /> </div> </Layout>
   )
 }
 
