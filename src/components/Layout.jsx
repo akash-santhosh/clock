af9c7134ab17301a5a651f7bb770eb005071d877
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2025-04-13 01:01:24 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,25 +3,15 @@ import { html } from 'hono/html'
 const Layout = (props) => html`<!DOCTYPE html>
   <html>
     <head>
-      <title>Screenly Clock App</title>
+      <title>Clock</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="preload" href="/static/fonts/Barlow-Regular.woff" as="font" crossOrigin="anonymous" />
       <link rel="stylesheet" href="/static/styles/main.css" />
-      <script
-        src="https://js.sentry-cdn.com/${props.sentryId}.min.js"
-        crossorigin="anonymous"
-      ></script>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=${props.gaId}"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${props.gaId}');
       </script>
       <script>
-        var country = '${props.country}';
+        var country = 'IN';
       </script>
       <script src="/static/js/main.js" async defer></script>
     </head>
